31b4c05e1702b4ff0527e11da0a08bfe781ebc06
HgermanDev <hgerman.developer@gmail.com>
2025-09-12 00:59:53 +0000

    Criado código por VSCODE em ambiente windows.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,66 @@
 #include <stdio.h>
+#include <windows.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+void counting()
+{
+    for (int i = 3; i > 0; i--)
+    {
+        printf("%d...\n", i);
+        Sleep(1000);
+    }
+}
+int main()
+{
+    int populacao[2], pturistico[2];
+    float area[2], pib[2];
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("SUPER TRUNFO\n CIDADES !\n \n \n Pressione enter_");
+    getchar();     // função de pausa para checagem do input
+    system("cls"); // limpeza da tela
+    counting();    // timer
+
+    printf("###########################\n");
+    printf("Digite os dados de sua carta 1\n");
+    printf("Populacao: ");
+    scanf("%d", &populacao[1]);
+    printf("Pontos turisticos: ");
+    scanf("%d", &pturistico[1]);
+    printf("Area: ");
+    scanf("%f", &area[1]);
+    printf("Pib: ");
+    scanf("%f", &pib[1]);
+
+    counting();    // timer
+    system("cls"); // limpeza da tela
+    printf("Pressione enter para registrar a carta 2");
+    getchar(); // função de pausa para checagem do input
+
+    printf("###########################\n");
+    printf("Digite os dados de sua carta 2\n");
+    printf("Populacao: ");
+    scanf("%d", &populacao[2]);
+    printf("Pontos turisticos: ");
+    scanf("%d", &pturistico[2]);
+    printf("Area: ");
+    scanf("%f", &area[2]);
+    printf("Pib: ");
+    scanf("%f", &pib[2]);
+    system("cls"); // limpeza da tela
+    counting();    // timer
+
+    printf("###########################\n");
+    printf("Dados de sua carta 1\n");
+    printf("\nPopulacao: %d", populacao[1]);
+    printf("\nPontos turisticos: %d", pturistico[1]);
+    printf("\nArea: %f", area[1]);
+    printf("\nPopulacao: %f", pib[1]);
+
+    printf("\n\n###########################\n");
+    printf("Dados de sua carta 2\n");
+    printf("\nPopulacao: %d", populacao[2]);
+    printf("\nPontos turisticos: %d", pturistico[2]);
+    printf("\nArea: %f", area[2]);
+    printf("\nPopulacao: %f", pib[2]);
 
     return 0;
 }
